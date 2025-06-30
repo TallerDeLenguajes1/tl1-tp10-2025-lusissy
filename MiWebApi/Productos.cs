529e2965ad9dd485a90b9612148a60c90e12ac35
Lourdes <lourdessissy@gmail.com>
2025-06-29 23:48:49 -0300

    terminado punto 3

---
+++ MiWebApi/Productos.cs
@@ -0,0 +1,12 @@
+namespace EspacioComercio
+{
+    public class Productos
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public double price { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public string image { get; set; }
+    }
+}
---
+++ MiWebApi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using EspacioComercio;
+
+HttpClient client = new HttpClient();
+string url = "https://fakestoreapi.com/products";
+
+string cuerpo = await client.GetStringAsync(url);
+
+List<Productos> producto = JsonSerializer.Deserialize<List<Productos>>(cuerpo);
+
+foreach (var productoAux in producto)
+{
+    Console.WriteLine("Nombre: " + productoAux.title);
+    Console.WriteLine("Categoria: " + productoAux.category);
+    Console.WriteLine("Precio: $" + productoAux.price);
+}
+
+string archivoJson = JsonSerializer.Serialize(producto, new JsonSerializerOptions { WriteIndented = true });
+File.WriteAllText("productos.json", archivoJson);
